b38bce093bc9a45be91dbd7f0b64ae22cc065896
Alex Kube <alexander.j.kube@gmail.com>
2019-04-06 00:12:46 +0430

    Add test program

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/queue-b/go-mavlink2"
+)
+
+func main() {
+	udp, err := net.ListenPacket("udp4", "0.0.0.0:14551")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+	runloop:
+		for {
+			select {
+			case <-sigs:
+				break runloop
+			default:
+				packet := make([]byte, 1024)
+				length, addr, err := udp.ReadFrom(packet)
+
+				fmt.Println(length)
+				fmt.Println(addr)
+
+				if err != nil {
+					fmt.Println(err)
+					break runloop
+				}
+
+				frame, err := mavlink.FrameFromBytes(packet[:length])
+
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				fmt.Println(frame)
+			}
+		}
+
+		udp.Close()
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
