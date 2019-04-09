ab8b34492e96307eb37e0c2c29e0fca366ebf1e0
Alex Kube <alexander.j.kube@gmail.com>
2019-04-09 13:59:19 +0430

    Remove debugging

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 
 	mavlink2 "github.com/queue-b/go-mavlink2"
-	"github.com/queue-b/go-mavlink2/ardupilotmega"
 	"github.com/queue-b/go-mavlink2/common"
-	"github.com/queue-b/go-mavlink2/icarous"
-	"github.com/queue-b/go-mavlink2/uAvionix"
 )
 
 func main() {
@@ -23,56 +18,50 @@ func main() {
 		return
 	}
 
-	sigs := make(chan os.Signal, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	readPipe, writePipe := io.Pipe()
 
 	var wg sync.WaitGroup
 
-	dialects := mavlink2.Dialects([]mavlink2.Dialect{common.DialectCommon{}, ardupilotmega.DialectArdupilotmega{}, uAvionix.DialectUavionix{}, icarous.DialectIcarous{}})
+	dialects := mavlink2.Dialects([]mavlink2.Dialect{common.DialectCommon{}})
 
-	wg.Add(1)
+	stream := mavlink2.MAVLinkStream{}
+
+	messages := make(chan mavlink2.Message)
+	frames := make(chan mavlink2.Frame)
+
+	stream.Dialects = dialects
+	stream.Messages = messages
+	stream.Frames = frames
+
+	stream.Run(readPipe)
+
+	wg.Add(2)
 	go func() {
-	runloop:
-		for {
-			select {
-			case <-sigs:
-				break runloop
-			default:
-				packet := make([]byte, 1024)
-				length, _, err := udp.ReadFrom(packet)
-
-				if err != nil {
-					fmt.Println(err)
-					break runloop
-				}
-
-				frame, err := mavlink2.FrameFromBytes(packet[:length])
-
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-
-				err = dialects.Validate(frame)
-
-				if err != nil && err != mavlink2.ErrUnknownMessage {
-					fmt.Printf("*** Error validating %v\n", err)
-					fmt.Println(frame)
-				}
-
-				message, err := dialects.GetMessage(frame)
-
-				if err != nil {
-					fmt.Printf("*** Error parsing %v\n", err)
-				}
-
-				fmt.Println(message)
-			}
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
-		udp.Close()
-		wg.Done()
+		for {
+			packet := make([]byte, 1024)
+			length, _, err := udp.ReadFrom(packet)
+
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			writePipe.Write(packet[:length])
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case frame := <-frames:
+				fmt.Println(frame)
+			}
+		}
 	}()
 
 	wg.Wait()
