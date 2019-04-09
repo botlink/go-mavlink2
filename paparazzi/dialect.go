27722fe9ad5f2cb540d257ed50be627454926272
Alex Kube <alexander.j.kube@gmail.com>
2019-04-09 16:54:21 +0430

    Add paparazzi dialect

---
+++ paparazzi/dialect.go
@@ -0,0 +1,103 @@
+package paparazzi
+
+import mavlink2 "github.com/queue-b/go-mavlink2"
+
+/*
+Generated using mavgen - https://github.com/ArduPilot/pymavlink/
+
+Copyright 2019 queue-b <https://github.com/queue-b>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of the generated software (the "Generated Software"), to deal
+in the Generated Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Generated Software, and to permit persons to whom the Generated
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Generated Software.
+
+THE GENERATED SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE GENERATED SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE GENERATED SOFTWARE.
+*/
+
+// Dialect represents a collection of MAVLink messages
+type Dialect struct{}
+
+// GetName gets the name of the Dialect
+func (d Dialect) GetName() string {
+	return "paparazzi"
+}
+
+// GetMeta retrieves the metadata for the message. If no metadata is found, ErrUnknownMessage is returned
+func (d Dialect) GetMeta(messageID uint32) (meta mavlink2.MessageMeta, err error) {
+	var ok bool
+
+	if meta, ok = paparazziMessages[messageID]; !ok {
+		err = mavlink2.ErrUnknownMessage
+	}
+
+	return
+}
+
+// GetMessage extracts and parses the message contained in the Frame
+func (d Dialect) GetMessage(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+	if parser, ok := paparazziParsers[frame.GetMessageID()]; ok {
+		message, err = parser(frame)
+		return
+	}
+
+	err = mavlink2.ErrUnknownMessage
+	return
+}
+
+var paparazziMessages = map[uint32]mavlink2.MessageMeta{
+	180: mavlink2.MessageMeta{CRCExtra: 231, MinimumLength: 54, MaximumLength: 54},
+	181: mavlink2.MessageMeta{CRCExtra: 129, MinimumLength: 4, MaximumLength: 4},
+	182: mavlink2.MessageMeta{CRCExtra: 115, MinimumLength: 2, MaximumLength: 2},
+	183: mavlink2.MessageMeta{CRCExtra: 186, MinimumLength: 4, MaximumLength: 4},
+	184: mavlink2.MessageMeta{CRCExtra: 40, MinimumLength: 2, MaximumLength: 2},
+}
+
+var paparazziParsers = map[uint32]mavlink2.FrameParser{
+	180: mavlink2.FrameParser(func(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+		message = &ScriptItem{}
+
+		err = message.Read(frame)
+
+		return
+	}),
+	181: mavlink2.FrameParser(func(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+		message = &ScriptRequest{}
+
+		err = message.Read(frame)
+
+		return
+	}),
+	182: mavlink2.FrameParser(func(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+		message = &ScriptRequestList{}
+
+		err = message.Read(frame)
+
+		return
+	}),
+	183: mavlink2.FrameParser(func(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+		message = &ScriptCount{}
+
+		err = message.Read(frame)
+
+		return
+	}),
+	184: mavlink2.FrameParser(func(frame mavlink2.Frame) (message mavlink2.Message, err error) {
+		message = &ScriptCurrent{}
+
+		err = message.Read(frame)
+
+		return
+	}),
+}
---
+++ paparazzi/script_item.go
@@ -0,0 +1,217 @@
+package paparazzi
+
+/*
+Generated using mavgen - https://github.com/ArduPilot/pymavlink/
+
+Copyright 2019 queue-b <https://github.com/queue-b>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of the generated software (the "Generated Software"), to deal
+in the Generated Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Generated Software, and to permit persons to whom the Generated
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Generated Software.
+
+THE GENERATED SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE GENERATED SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE GENERATED SOFTWARE.
+*/
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"strings"
+	"text/tabwriter"
+
+	mavlink2 "github.com/queue-b/go-mavlink2"
+	"github.com/queue-b/go-mavlink2/util"
+)
+
+/*ScriptItem Message encoding a mission script item. This message is emitted upon a request for the next script item. */
+type ScriptItem struct {
+	/*Seq Sequence */
+	Seq uint16
+	/*TargetSystem System ID */
+	TargetSystem uint8
+	/*TargetComponent Component ID */
+	TargetComponent uint8
+	/*Name The name of the mission script, NULL terminated. */
+	Name [50]byte
+	/*HasExtensionFieldValues indicates if this message has any extensions and  */
+	HasExtensionFieldValues bool
+}
+
+func (m *ScriptItem) String() string {
+	var builder strings.Builder
+	var buffer bytes.Buffer
+
+	writer := tabwriter.NewWriter(&buffer, 0, 0, 2, ' ', 0)
+
+	builder.WriteString("Name:\t%v/%v\n")
+	// Output field values based on the decoded message type
+	builder.WriteString("Seq:\t%v \n")
+	builder.WriteString("TargetSystem:\t%v \n")
+	builder.WriteString("TargetComponent:\t%v \n")
+	builder.WriteString("Name:\t%v \n")
+	format := builder.String()
+
+	fmt.Fprintf(
+		writer,
+		format,
+		m.GetDialect(),
+		m.GetMessageName(),
+		m.Seq,
+		m.TargetSystem,
+		m.TargetComponent,
+		m.Name,
+	)
+
+	writer.Flush()
+	return string(buffer.Bytes())
+}
+
+// SetName encodes the input string to the Name array
+func (m *ScriptItem) SetName(input string) (err error) {
+	clen := int(math.Min(float64(len(input)), float64(50)))
+	copy(m.Name[:], []byte(input)[:clen])
+
+	if len(input) > 50 {
+		err = mavlink2.ErrStringTooLong
+	}
+
+	return
+}
+
+// GetName decodes the null-terminated string in the Name
+func (m *ScriptItem) GetName() string {
+	clen := util.CStrLen(m.Name[:])
+
+	return string(m.Name[:clen])
+}
+
+// GetVersion gets the MAVLink version of the Message contents
+func (m *ScriptItem) GetVersion() int {
+	if m.HasExtensionFieldValues {
+		return 2
+	}
+
+	return 1
+}
+
+// GetDialect gets the name of the dialect that defines the Message
+func (m *ScriptItem) GetDialect() string {
+	return "paparazzi"
+}
+
+// GetMessageName gets the name of the Message
+func (m *ScriptItem) GetMessageName() string {
+	return "ScriptItem"
+}
+
+// GetID gets the ID of the Message
+func (m *ScriptItem) GetID() uint32 {
+	return 180
+}
+
+// HasExtensionFields returns true if the message definition contained extensions; false otherwise
+func (m *ScriptItem) HasExtensionFields() bool {
+	return false
+}
+
+func (m *ScriptItem) getV1Length() int {
+	return 54
+}
+
+func (m *ScriptItem) getIOSlice() []byte {
+	return make([]byte, 54+1)
+}
+
+// Read sets the field values of the message from the raw message payload
+func (m *ScriptItem) Read(frame mavlink2.Frame) (err error) {
+	version := frame.GetVersion()
+
+	// Ensure only Version 1 or Version 2 were specified
+	if version != 1 && version != 2 {
+		err = mavlink2.ErrUnsupportedVersion
+		return
+	}
+
+	// Don't attempt to Read V2 messages from V1 frames
+	if m.GetID() > 255 && version < 2 {
+		err = mavlink2.ErrDecodeV2MessageV1Frame
+		return
+	}
+
+	// binary.Read can panic; swallow the panic and return a sane error
+	defer func() {
+		if r := recover(); r != nil {
+			err = mavlink2.ErrPrivateField
+		}
+	}()
+
+	// Get a slice of bytes long enough for the all the ScriptItem fields
+	// binary.Read requires enough bytes in the reader to read all fields, even if
+	// the fields are just zero values. This also simplifies handling MAVLink2
+	// extensions and trailing zero truncation.
+	ioSlice := m.getIOSlice()
+
+	copy(ioSlice, frame.GetMessageBytes())
+
+	// Indicate if
+	if version == 2 && m.HasExtensionFields() {
+		ioSlice[len(ioSlice)-1] = 1
+	}
+
+	reader := bytes.NewReader(ioSlice)
+
+	err = binary.Read(reader, binary.LittleEndian, m)
+
+	return
+}
+
+// Write encodes the field values of the message to a byte array
+func (m *ScriptItem) Write(version int) (output []byte, err error) {
+	var buffer bytes.Buffer
+
+	// Ensure only Version 1 or Version 2 were specified
+	if version != 1 && version != 2 {
+		err = mavlink2.ErrUnsupportedVersion
+		return
+	}
+
+	// Don't attempt to Write V2 messages to V1 bodies
+	if m.GetID() > 255 && version < 2 {
+		err = mavlink2.ErrEncodeV2MessageV1Frame
+		return
+	}
+
+	err = binary.Write(&buffer, binary.LittleEndian, *m)
+	if err != nil {
+		return
+	}
+
+	// V1 uses fixed message lengths and does not include any extension fields
+	// Truncate the byte slice to the correct length
+	if version == 1 {
+		output = buffer.Bytes()[:m.getV1Length()]
+	}
+
+	// V2 uses variable message lengths and includes extension fields
+	// The variable length is caused by truncating any trailing zeroes from
+	// the end of the message before it is added to a frame
+	if version == 2 {
+		output = util.TruncateV2(buffer.Bytes())
+	}
+
+	return
+
+}
